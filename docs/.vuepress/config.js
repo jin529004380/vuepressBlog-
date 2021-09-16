44df98c8ba3653ede92c4d19d9eb9074cdf55b22
jincheng <15250125863@163.com>
2021-09-16 19:30:24 +0800

    主页的调整+Markdown语法编辑

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
     description: "我的个人网站",
     head: [
         // 注入到当前页面的 HTML <head> 中的标签
-        ["link", { rel: "icon", href: "/favicon.ico" }], // 增加一个自定义的 favicon(网页标签的图标)
+        ["link", { rel: "icon", href: "/favicon1.ico" }], // 增加一个自定义的 favicon(网页标签的图标)
     ],
     base: "/", // 这是部署到github相关的配置
     markdown: {
@@ -22,6 +22,7 @@ module.exports = {
                 ],
             },
             { text: "算法题库", link: "/algorithm/" },
+            { text: "工具", link: "/util/" },
             { text: "微博", link: "https://baidu.com" },
         ],
         sidebar: {
@@ -45,6 +46,7 @@ module.exports = {
                 },
             ],
             "/language/japanese/": [""],
+            "/util/": [""]
         },
         sidebarDepth: 1, //默认1,显示h1、h2;设置2,显示h1、h2、h3
         // displayAllHeaders: true //显示所有页面的标题链接
